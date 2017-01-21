a99df3f45f7f5e40f19bd3c1842372ea56c07541
nerdfiles <nerdfiles@gmail.com>
2017-01-20 22:21:29 -0600

    Cleanup.

--- src/mailer.js
+++ src/mailer.js
@@ -13,14 +13,14 @@ import colors from 'colors';
 var Curl = C.Curl;
 
 module.exports = function (testMode) {
-  var recipientList, htmlEmail, textEmail, textSubject, from;
+  let recipientList, htmlEmail, textEmail, textSubject, from;
 
-  var _HTTPHEADER = JSON.parse(fs.readFileSync('./HTTPHEADER', 'utf-8'));
-  var testRecipientsList = fs.readFileSync('./TEST_RECIPIENT', 'utf-8');
-  var _testRecipientsList = testRecipientsList.trim();
-  var __testRecipient = [{ "address": _testRecipientsList }];
-  var loadedRecipientsList = fs.readFileSync('./grouped.deduped.txt', 'utf-8');
-  var _recipientList = loadedRecipientsList.split('\n');
+  const _HTTPHEADER = JSON.parse(fs.readFileSync('./HTTPHEADER', 'utf-8'));
+  const testRecipientsList = fs.readFileSync('./TEST_RECIPIENT', 'utf-8');
+  const _testRecipientsList = testRecipientsList.trim();
+  const __testRecipient = [{ "address": _testRecipientsList }];
+  const loadedRecipientsList = fs.readFileSync('./grouped.deduped.txt', 'utf-8');
+  const _recipientList = loadedRecipientsList.split('\n');
 
   if (testMode) {
     console.log('Loading test e-mail recipient!');
@@ -39,7 +39,7 @@ module.exports = function (testMode) {
   htmlEmail = fs.readFileSync('./dist/basic.html', 'utf-8');
   from = fs.readFileSync('./FROM', 'utf-8');
 
-  var initData = function () {
+  const initData = function () {
     var data = {
       "content": {
         "from"    : from.trim(),
@@ -53,9 +53,9 @@ module.exports = function (testMode) {
   };
 
 
-  var curl = new Curl();
-  var url  = 'https://api.sparkpost.com/api/v1/transmissions';
-  var data = initData();
+  const curl = new Curl();
+  const url  = 'https://api.sparkpost.com/api/v1/transmissions';
+  let data = initData();
 
   data = JSON.stringify(data);
 
@@ -68,7 +68,7 @@ module.exports = function (testMode) {
 
   curl.on('end', function (statusCode, body) {
     try {
-      var _body = JSON.parse(body);
+      const _body = JSON.parse(body);
       console.log("ID: " + _body.results.id.toString().rainbow);
       console.log("Bounces: " + _body.results.total_rejected_recipients.toString().rainbow);
       console.log("Successes: " + _body.results.total_accepted_recipients.toString().rainbow);
